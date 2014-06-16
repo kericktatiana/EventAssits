830eb827b4ff06339b656b51f859b75151eff470
Tatiana Kerick <KerickTatiana@gmail.com>
2014-06-16 18:20:39 -0400

    deleting events, starting over, less confusion

--- app/scripts/controllers/dashboard.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('angularFullApp')
-  .controller('DashboardCtrl', function ($scope, $http) {
-    $http.get('/api/awesomeThings').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-    });
-  });
