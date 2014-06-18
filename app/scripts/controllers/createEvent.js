7ad0875737a0bb996a99a6fb1c2113f98ed7f198
Tatiana Kerick <KerickTatiana@gmail.com>
2014-06-18 02:33:40 -0400

    newer files

---
+++ app/scripts/controllers/createEvent.js
@@ -0,0 +1,44 @@
+'use strict';
+
+/**
+ * Event.js 
+ * controls the events and gathers segments
+**/
+
+angular.module('angularFullApp')
+	.controller('EventCtrl', function ($scope, $http) {
+		$http.get('/api/awesomeThings').success(
+			
+			$scope.createEvent = function(form) {
+				$scope.submitted = true;
+
+				if(form.$valid) {
+					//need to create event service?
+					Event.createEvent({
+						title: $scope.event.title,
+						date: $scope.event.date,
+						setUp: $scope.event.setUp,
+						startTime: $scope.event.startTime,
+						strike: $scope.event.strike,
+						description: $scope.event.description
+					})
+					.then( function() {
+						//Event created, redirect them to event page
+						$location.path('/event');
+					})
+					.catch( function(err) {
+						err = err.data;
+						$scope.error = {};
+
+						//update validity of form fields that match the mongoose errors
+						angular.forEach(err.errors, function(error, field) {
+							form[field].$setValidity('mongoose', false);
+							$scope.errors[field] = error.message;
+						});
+					});
+				}
+			});//createEvent
+
+
+
+	});//controller
