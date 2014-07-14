aa67a653d88d24a7a562ea6c67928d77c00bce39
Tatiana Kerick <KerickTatiana@gmail.com>
2014-07-13 20:44:07 -0400

    dashboard dates human readable

--- app/scripts/controllers/dashboard.js
+++ app/scripts/controllers/dashboard.js
@@ -14,5 +14,59 @@ angular.module('angularFullApp')
 
 				$scope.getEvents = getEvents;
 
+				for(var i = 0; i < getEvents.length; i++) {
+					var event = getEvents[i];
+
+					var eventDate = event.date;
+
+					var date = new Date(eventDate);
+					var day = date.getDate()+1;
+					var month = date.getMonth()+1;
+					var year = date.getFullYear();
+
+					//convert 0-11 months to strings
+					switch (new Date().getMonth()) {
+						case 0:
+							month = 'Janurary';
+							break;
+						case 1:
+							month = 'Feburary';
+							break;
+						case 2:
+							month = 'March';
+							break;
+						case 3:
+							month = 'April';
+							break;
+						case 4:
+							month = 'May';
+							break;
+						case 5:
+							month = 'June';
+							break;
+						case 6:
+							month = 'July';
+							break;
+						case 7:
+							month = 'August';
+							break;
+						case 8:
+							month = 'September';
+							break;
+						case 9:
+							month = 'October';
+							break;
+						case 10:
+							month = 'November';
+							break;
+						case 11:
+							month = 'December';
+							break;
+					}//switch
+
+					//set mongo date to human
+					event.date = month + ' ' + day + ' ' + year;
+				}
+
 			});
 	});
