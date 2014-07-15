98dbfffc3adafa567fbd008ee8ead80602a7fe47
Tatiana Kerick <KerickTatiana@gmail.com>
2014-07-15 16:42:25 -0400

    event css and segments getting there, segment list fancy, icons added

--- app/scripts/controllers/event.js
+++ app/scripts/controllers/event.js
@@ -145,7 +145,7 @@ angular.module('angularFullApp')
 
 				if (time.length > 1) { // If time format correct
 					time = time.slice (1);  // Remove full string match value
-					time[5] = +time[0] < 12 ? 'AM' : 'PM'; // Set AM/PM
+					time[5] = +time[0] < 12 ? ' AM' : ' PM'; // Set AM/PM
 					time[0] = +time[0] % 12 || 12; // Adjust hours
 				}
 				return time.join (''); // return adjusted time or original string
