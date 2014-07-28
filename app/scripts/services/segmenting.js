09ff8212d32258d3cd1676bd0da570559bd6f0d8
Tatiana Kerick <KerickTatiana@gmail.com>
2014-07-28 01:40:23 -0400

    segment service include edit/delete

--- app/scripts/services/segmenting.js
+++ app/scripts/services/segmenting.js
@@ -32,6 +32,48 @@ angular.module('angularFullApp')
 					}).$promise;
 			},
 
+
+
+			/**
+			 * Edit Segment
+			 *
+			 * @param - {String} - newTime
+			 * @param - {String} - newDuration
+			 * @param - {String} - newCategory
+			 * @param - {String} - newTask
+			**/
+			editSegment: function(newTime, newDuration, newCategory, newTask, callback) {
+				var cb = callback || angular.noop;
+
+				return Segment.update({
+					newTime: newTime,
+					newDuration: newDuration,
+					newCategory: newCategory,
+					newTask: newTask
+				}, function(segment) {
+					return cb(segment);
+				}, function(err) {
+					return cb(err);
+				}).$promise;
+			},
+
+
+			/** 
+			 * Delete Segment
+			 * 
+			 * @param - {String} - segmentId
+			**
+			deleteSegment: function(segmentId, callback) {
+				var cb = callback || angular.noop;
+
+				delete functionality here
+			}**/
+
+
+			currentSegment: function() {
+				return Segment.get();
+			},
+
 		};//return
 
 	});//factory
